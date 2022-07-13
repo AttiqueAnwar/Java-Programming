466b02e26f72e9e7af9e0dbeeb68439fc56cb432
dell <dell@DESKTOP-69FE769>
2022-07-13 12:54:05 +0500

    First Commit

---
+++ MyJavaApp/src/Main.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub		
+
+		/*	for(int i=0 ;i<10 ; i++ ) {
+				Scanner input = new Scanner(System.in);
+
+				System.out.println("Enter a decimal number: ");
+				
+				double number = input.nextDouble();
+
+				System.out.println("Your number is: " + number +" "+ i);
+				
+			
+			}*/
+		
+		
+		/*for (int i=6; i>0 ; i-- ) {
+			
+			for (int j=1 ; j<i ; j++ ) {
+				System.out.printf("1");
+				
+			}
+			System.out.println("2");
+		}
+		System.out.println("*");
+		
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a decimal number: ");
+		 String text = 	input.next();
+		 System.out.println(text);
+		 
+		 */
+		 
+		
+		
+		 
+		 
+			 }
+			
+	
+	
+}
---
+++ Scanner/src/Application.java
@@ -0,0 +1,14 @@
+
+public class Application{
+
+	public static void main(String[] args){
+		
+		
+
+		System.out.println("Enter a decimal number: ");
+		
+		
+
+		System.out.println("Your number is: " );
+	}
+}
